4b62e486ea4d0bd7b73fba750623c088ec51041f
mementoz <135979733+Mementozzz@users.noreply.github.com>
2025-12-05 14:56:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import argparse
 from solver.solver import ClockSolver
 
 if __name__ == "__main__":
-    solver = ClockSolver()
+    parser = argparse.ArgumentParser(description='Chronos Clock Solver')
+    parser.add_argument('-v', '--verbose', action='store_true', 
+                        help='Enable verbose output with debug information')
+    parser.add_argument('--no-window', action='store_true',
+                        help='Run without GUI window (headless mode)')
+    
+    args = parser.parse_args()
+    
+    solver = ClockSolver(verbose=args.verbose, show_window=not args.no_window)
     solver.run()
